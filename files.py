8cccf6da6b489817fc50cc27e74f5d117e49ad28
Julian Camargo <jcamargo2@gmail.com>
2011-10-11 04:56:26 -0500

    Renombramiento de una variable para mayor claridad

--- files.py
+++ files.py
@@ -41,9 +41,9 @@ def cargarcapacidadcosto(f, n):
 	s = []
 	for i in range(n):
 		junk = f.readline().split()
-		n = int(junk[0])
+		c = int(junk[0])
 		costo, tiempo = [int(x) for x in junk[-1].split('-')]
-		s.append({'capacidad': n, 'costo': costo,  'tiempo':tiempo})
+		s.append({'capacidad': c, 'costo': costo,  'tiempo':tiempo})
 	return s
 
 """
